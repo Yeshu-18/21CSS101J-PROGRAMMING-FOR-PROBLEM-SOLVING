98821f628d716660096a455c6cdd29ea7bd588b1
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-08 17:13:03 +0530

    Create Data type ( character ) .c

---
+++ Data type ( character ) .c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+   char character;
+   printf("Enter the character : ");
+   scanf("%c", &character);
+   printf("The character you entered is = %c", character);
+}

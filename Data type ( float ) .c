7c8b7f7c730e3900a222278e0e902b8c8b2fe37c
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-08 17:15:43 +0530

    Create Data type ( float ) .c

---
+++ Data type ( float ) .c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+   float inputfloat;
+   printf("Enter the float value : ");
+   scanf("%f", &inputfloat);
+   printf("The float value you entered is = %f", inputfloat);
+}

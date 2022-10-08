8afdaa28a7b2c494ad98ac98c84d1cf132abab6d
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-08 17:16:46 +0530

    Create Data type ( float with precision) .c

---
+++ Data type ( float with precision) .c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+   float inputfloat;
+   printf("Enter the float value : ");
+   scanf("%f", &inputfloat);
+   printf("The float value you entered is = %.2f", inputfloat);
+}

8e4fc0d0134d1a4b6ecbfc9a955edcbdbfd88060
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-08 18:05:33 +0530

    Create Size of combination of operators and data types.c

---
+++ Size of combination of operators and data types.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+ double num1=12.3 , num2=12.3442;   
+ printf("Size of the numerical expression (combination of operators and data types) = %ld\n", sizeof(num1 + num2 + num1+1*89438975/2.3));
+ return 0;
+}

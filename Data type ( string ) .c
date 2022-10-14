3f8422a691ef5379b6c02b031d9253fbf8e234ff
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 18:39:46 +0530

    Update Data type ( string ) .c

--- Data type ( string ) .c
+++ Data type ( string ) .c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+    printf("Roll number = RA2211042010017\n");
     char string[10];
     printf("Enter the string (upto 10 characters) : ");
     scanf("%s", string);

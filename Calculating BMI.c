518067467dff07a81b003f85b4ca81804200744b
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 18:23:20 +0530

    Update Calculating BMI.c

--- Calculating BMI.c
+++ Calculating BMI.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(){
+    printf("Roll number = RA2211042010017\n");
     float height,weight,bmi;
     printf("Enter the height (in metres) : ");
     scanf("%f", &height);
     printf("Enter the weight (in kgs) : ");
     scanf("%f", &weight);
     bmi=weight/(height*height);
-    printf("The BMI index is : %.2f",bmi);
+    printf("The BMI index is = %.2f",bmi);
 }

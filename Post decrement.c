a21ee8684c3eafaa78c4793f3eee66fb3902a511
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 20:03:29 +0530

    Update Post decrement.c

--- Post decrement.c
+++ Post decrement.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 main()
 {
+  printf("Roll number = RA2211042010017\n");
   int a=3;
   printf("The value of a returned after post decrement is = %d",a--);
   printf("\nThe value of a now is = %d",a);

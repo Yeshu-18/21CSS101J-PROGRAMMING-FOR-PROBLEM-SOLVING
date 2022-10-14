56e114e1ddd92b03a95fac6bf62863d1463dcc5c
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 19:11:36 +0530

    Update Finding ascii value of a number.c

--- Finding ascii value of a number.c
+++ Finding ascii value of a number.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {  
+    printf("Roll number = RA2211042010017\n");
     char c;
     printf("Enter a character: ");
     scanf("%c",&c);  
-    printf("ASCII value of %c = %d",c,c);   
-    return 0;
+    printf("ASCII value of %c = %d",c,c); 
 }

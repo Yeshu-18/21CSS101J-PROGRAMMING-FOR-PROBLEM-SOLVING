d8802d3c812651a9da8fc79fddd9ba84696ebb3c
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 20:30:49 +0530

    Update Sum of N natural integers.c

--- Sum of N natural integers.c
+++ Sum of N natural integers.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 int main() 
 {
+    printf("Roll number = RA2211042010017\n");
     int n, i, sum = 0;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-
     for (i = 1; i <= n; ++i) 
         sum += i;
-
     printf("Sum = %d", sum);
     return 0;
 }

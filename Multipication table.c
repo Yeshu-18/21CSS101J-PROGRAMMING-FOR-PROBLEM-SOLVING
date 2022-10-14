8e68ee8b419526c12606192ff7104ba711f0c385
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 19:33:47 +0530

    Update Multipication table.c

--- Multipication table.c
+++ Multipication table.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 int main() {
+  printf("Roll number = RA2211042010017\n");
   int n, i;
   printf("Enter an integer: ");
   scanf("%d", &n);
   for (i = 1; i <= 10; ++i) 
   printf("%d * %d = %d \n", n, i, n * i);
-  
-  return 0;
 }

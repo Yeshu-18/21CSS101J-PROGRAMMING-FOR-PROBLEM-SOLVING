1fa75a2ff3674b89c5c66abbad022fd2f646b788
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 20:24:16 +0530

    Update Size of combination of operators and data types.c

--- Size of combination of operators and data types.c
+++ Size of combination of operators and data types.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+ printf("Roll number = RA2211042010017\n");
  double num1=12.3 , num2=12.3442;   
  printf("Size of the numerical expression (combination of operators and data types) = %ld\n", sizeof(num1 + num2 + num1+1*89438975/2.3));
  return 0;

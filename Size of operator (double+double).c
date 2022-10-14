2f4b2f24ae8ccee497b8279fa3e683fcaab4c42f
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 20:25:16 +0530

    Update Size of operator (double+double).c

--- Size of operator (double+double).c
+++ Size of operator (double+double).c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+ printf("Roll number = RA2211042010017\n");
  double num1=12.3 , num2=12.3442;   
  printf("Size of the numerical expression (double and double) = %ld\n", sizeof(num1 + num2));
  return 0;

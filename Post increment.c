70d547bf3952b93d8d73c0bd71ecffb60e89e45b
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 20:04:33 +0530

    Update Post increment.c

--- Post increment.c
+++ Post increment.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 main()
 {
+printf("Roll number = RA2211042010017\n");
 int a=2;
 printf("The value returned after post increment of a is = %d",a++);
 printf("\nThe value of a now is = %d",a);

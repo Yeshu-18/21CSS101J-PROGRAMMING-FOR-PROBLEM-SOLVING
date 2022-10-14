4be40fb8be2c3cf3ae04ffe01205e711763f674b
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 18:48:01 +0530

    Update Find largest of three numbers.c

--- Find largest of three numbers.c
+++ Find largest of three numbers.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
-
 int main( ) {
-
-  double n1, n2, n3;
-
+  printf("Roll number = RA2211042010017\n");
+  int n1, n2, n3;
   printf("Enter three numbers: ");
-  scanf("%lf %lf %lf", &n1, &n2, &n3);
-
-  // if n1 is greater than both n2 and n3, n1 is the largest
+  scanf("%d %d %d", &n1, &n2, &n3);
   if (n1 >= n2 && n1 >= n3)
-    printf("%.2lf is the largest number.", n1);
-
-  // if n2 is greater than both n1 and n3, n2 is the largest
+    printf("The largest number is = %d", n1);
   else if (n2 >= n1 && n2 >= n3)
-    printf("%.2lf is the largest number.", n2);
-
-  // if both above conditions are false, n3 is the largest
+    printf("%The largest number is = %d", n2);
   else
-    printf("%.2lf is the largest number.", n3);
-
-  return 0;
+    printf("The largest number is = %d", n3);
 }

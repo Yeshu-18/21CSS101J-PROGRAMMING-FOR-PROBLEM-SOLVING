c2db6d271fd93490593817577911297e55db1283
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 20:13:57 +0530

    Update Print n natural numbers.c

--- Print n natural numbers.c
+++ Print n natural numbers.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main()
 {
+  printf("Roll number = RA2211042010017\n");
   int last_digit,i;
-    printf("The number of natural numbers to be displayed are:");
-    scanf("%d",&last_digit);
-
-    for (i=1;i<=last_digit;i++)
-    printf("%d\n",i);
-    
-  return 0;
+  printf("The number of natural numbers to be displayed are:");
+  scanf("%d",&last_digit);
+  for (i=1;i<=last_digit;i++)
+  printf("%d\n",i);
 }

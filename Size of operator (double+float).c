0f784b943997bac653515df72f33c6fcf3b100ba
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-14 20:25:33 +0530

    Update Size of operator (double+float).c

--- Size of operator (double+float).c
+++ Size of operator (double+float).c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+ printf("Roll number = RA2211042010017\n");
  double num1=12.3;   
  printf("Size of the numerical expression (double and float) = %ld\n", sizeof(num1+4.344));
  return 0;

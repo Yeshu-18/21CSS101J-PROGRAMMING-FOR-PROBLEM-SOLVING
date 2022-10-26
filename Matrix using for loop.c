9b3e4f234ea40cc594f2efe3d470a2340188f448
Yeshu-18 <113741529+Yeshu-18@users.noreply.github.com>
2022-10-26 09:58:11 +0530

    Update and rename matrix using for loop.c to Matrix using for loop.c

--- Matrix using for loop.c
+++ Matrix using for loop.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main() {
+    printf("Register number = %s", "RA2211042010017\n");
     int i;    //i is the number to be printed in the matrix
     for (i=1;i<=4;i++)
         printf("%d %d %d\n",i,i+1,i+2);
